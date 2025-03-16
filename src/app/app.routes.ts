0641b20eefd4f4866efb4fa7f269e990fdd83927
Zatt010 <afuheho1@gmail.com>
2025-03-16 17:35:32 -0400

    Landing Page implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { landingPage } from './pages/landingPage/landingPage.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent }, 
     { path: 'signup', component: SignupComponent },
+    { path: 'home', component: landingPage },
     { path: '', redirectTo: '/login', pathMatch: 'full' } 
   ];
