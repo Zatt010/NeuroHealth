88d723d3d31b7259ddb45bd814b7823e2731e59f
Zatt010 <afuheho1@gmail.com>
2025-05-07 16:05:13 -0400

    Agregar workflows de despliegue automático con Firebase Hosting8

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -24,7 +24,7 @@ export const routes: Routes = [
     component: CommunityComponent,
     children: [
       { path: 'new', component: NewPostComponent },
-      { path: ':id', component: PostDetailComponent, data: { renderMode: 'client' } }, 
+      //{ path: ':id', component: PostDetailComponent, data: { renderMode: 'client' } }, 
       { path: '', redirectTo: 'list', pathMatch: 'full' }
     ]
   },
